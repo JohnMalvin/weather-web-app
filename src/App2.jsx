062c5a8be03cba9732e499527e76f511184d3ee9
JohnMalvin <johnmalvin068@gmail.com>
2025-05-15 23:38:07 +1000

    added env file

---
+++ src/App2.jsx
@@ -0,0 +1,10 @@
+const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+console.log(WEATHER_API_KEY);
+function App2() {
+  return (
+    <div>
+      <h1>My App</h1>
+    </div>
+  );
+}
+export default App2;
