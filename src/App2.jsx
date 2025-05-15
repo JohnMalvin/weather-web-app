068a88fa53cf10c274accf130a0affc767f72160
JohnMalvin <johnmalvin068@gmail.com>
2025-05-16 00:10:42 +1000

    try fetch

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
+import { getTempinCel } from "./CLIENT_HELPER.JS";
+
 const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 console.log(WEATHER_API_KEY);
 function App2() {
+    const [temp, setTemp] = useState(0);
+    useEffect(() => {
+        fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/SYDNEY?unitGroup=us&include=minutes%2Cevents%2Calerts%2Ccurrent&key=AG4CQFT7CK4A6DBLTPQQ7PMB7&contentType=json", {
+            "method": "GET",
+            "headers": {
+            }
+            })
+          .then(response => {
+            console.log(response);
+          })
+          .catch(err => {
+            console.error(err);
+          });
+    }, []);
   return (
     <div>
-      <h1>My App</h1>
+        <h1>My App</h1>
+        <h2>Temperature in Sydney: {temp}°C</h2>
     </div>
   );
 }
