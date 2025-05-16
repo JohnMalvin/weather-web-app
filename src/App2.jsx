c24d5fd0ec2370bb296f262094dd57f434c848d4
JohnMalvin <johnmalvin068@gmail.com>
2025-05-16 21:31:23 +1000

    feat: enhance weather display with dynamic data fetching and improved styling

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-import { getWeatherData } from "./CLIENT_HELPER.JS";
 import Display2 from "./Display2";
 
 
@@ -7,24 +5,13 @@ import Display2 from "./Display2";
 export default function WeatherDisplay() {
 
 
-  useEffect(() => {
-    const fetchWeather = async () => {
-      try {
-        const weather = await getWeatherData("Sydney", 0, 0, false); // 26 hours from now, in °F
-        console.log(weather);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-  
-    fetchWeather();
-  }, []);
-
-
 
   return (
     <>
-      <Display2 />
+      <Display2
+        locationData="Sydney"
+        celciusData={false}
+      />
     </>
   );
 }
